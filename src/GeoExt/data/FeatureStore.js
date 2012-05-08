cb9e4169367a4c7c13de8caa7637ef74fea95f8c
Frederic Junod <frederic.junod@camptocamp.com>
2012-05-08 13:58:24 +0200

    Add FeatureStore

---
+++ src/GeoExt/data/FeatureStore.js
@@ -0,0 +1,88 @@
+Ext.define('GeoExt.data.FeatureStore', {
+    extend: 'Ext.data.Store',
+    requires: [
+
+    ],
+
+    // openlayers.layer.vector or layer record
+    layer: null,
+
+    constructor: function(config) {
+        config = Ext.apply({}, config);
+
+        var layer = config.layer;
+        delete config.layer;
+
+        if (config.features) {
+            config.data = config.features;
+        }
+        delete config.features;
+
+        this.callParent([config]);
+        if (layer) {
+            this.bind(layer);
+        }
+    },
+
+    bind: function(layer, options) {
+        if (this.layer) {
+            // already bound
+            return;
+        }
+        this.layer = layer;
+        // ...
+        this.on({
+            'load': this.onLoad,
+            'clear': this.onClear,
+            'add': this.onAdd,
+            'remove': this.onRemove,
+            'update': this.onUpdate,
+            scope: this
+        });
+        this.data.on({
+            'replace': this.onReplace,
+            scope: this
+        });
+
+        this.fireEvent("bind", this, this.layer);
+    },
+
+    unbind: function() {
+        if (this.layer) {
+            // ...
+            this.un({
+                'load': this.onLoad,
+                'clear': this.onClear,
+                'add': this.onAdd,
+                'remove': this.onRemove,
+                'update': this.onUpdate,
+                scope: this
+            });
+            this.data.un({
+                'replace': this.onReplace,
+                scope: this
+            });
+            this.layer = null;
+        }
+    },
+
+    onLoad: function() {
+
+    },
+    onClear: function() {
+
+    },
+    onAdd: function() {
+
+    },
+    onRemove: function() {
+
+    },
+    onUpdate: function() {
+
+    },
+    onReplace: function() {
+
+    }
+
+});
